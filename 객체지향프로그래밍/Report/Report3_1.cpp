680c439f3e0b2ea496c32c7d04f3602c7a3187ca
Choi-Ji-Yun <cjy5025@naver.com>
2020-08-18 00:33:32 +0900

    Class_Operator

---
+++ 객체지향프로그래밍/Report/Report3_1.cpp
@@ -0,0 +1,42 @@
+﻿/*#include <iostream>
+using namespace std;
+
+class coord {
+private:
+	int x, y;
+public:
+	coord() { x = 0; y = 0; }
+	coord(int i, int j) { x = i; y = j; }
+	void get_xy(int& i, int& j) { i = x; j = y; }
+	coord operator* (coord op); // *연산자중복 
+	coord operator/ (coord op); // /연산자중복
+}; 
+
+coord coord::operator*(coord op)
+{
+	coord temp;
+	temp.x = x * op.x;
+	temp.y = y * op.y;
+	return temp;
+} // * 중복연산자 정의
+
+coord coord::operator/(coord op)
+{
+	coord temp;
+	temp.x = x / op.x;
+	temp.y = y / op.y;
+	return temp;
+} // /중복연산자 정의
+
+int main()
+{
+	coord a(6, 8), b(3, 2), c; // 3개 객체 생성
+	int xx, yy;
+	c = a * b;
+	c.get_xy(xx, yy); // 곱한 결과 저장
+	cout << "*의 결과(x,y): " << xx << ',' << yy << endl; // 곱한 결과 출력
+	c = a / b;
+	c.get_xy(xx, yy); // 나눈 결과 저장
+	cout << "/의 결과(x,y): " << xx << ',' << yy << endl; // 나눈 결과 출력
+	return 0;
+}*/
