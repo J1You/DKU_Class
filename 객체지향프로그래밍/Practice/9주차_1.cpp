c1557b560a6fe8ac31d7674966678cb273ed57eb
Choi-Ji-Yun <cjy5025@naver.com>
2020-08-19 16:48:41 +0900

    Class

---
+++ 객체지향프로그래밍/Practice/9주차_1.cpp
@@ -0,0 +1,30 @@
+﻿/*
+//원점 거리 계산
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class point {
+private:
+	int x, y;
+public:
+	point(int i, int j)
+	{
+		x = i, y = j;
+	}
+	double origin_distance();
+};
+
+double point :: origin_distance()
+{
+	return sqrt(pow(x, 2) + pow(y, 2));
+}
+int main()
+{
+	point p1(3, 4), p2(-1, -5);
+	cout << "p1의 원점거리= " << p1.origin_distance() << endl;
+	cout << "p2의 원점거리= " << p2.origin_distance() << endl;
+	return 0;
+}
+*/
