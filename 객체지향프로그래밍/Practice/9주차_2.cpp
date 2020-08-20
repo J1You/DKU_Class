7e2929f2316b6429e0597581d53a591fed775153
Choi-Ji-Yun <cjy5025@naver.com>
2020-08-20 19:29:30 +0900

    Class

---
+++ 객체지향프로그래밍/Practice/9주차_2.cpp
@@ -0,0 +1,32 @@
+/*
+//좌표 사이 거리
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class point {
+private:
+	int x, y;
+public:
+	point(int i, int j)
+	{ 
+	double two_distance(point a);
+	friend double distance2(point a, point b);
+};
+double point::two_distance(point a)
+{
+	return sqrt(pow(x - a.x, 2) + pow(y - a.y, 2));
+}
+double distance2(point a, point b)
+{
+	return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+int main()
+{
+	point p1(3, 4), p2(-1, -5);
+	cout << "p1, p2 사이거리= " << p1.two_distance(p2) << endl;
+	cout << "p1, p2 사이거리= " << distance2(p1, p2) << endl;
+	return 0;
+}
+*/
