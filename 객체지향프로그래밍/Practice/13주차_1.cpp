0d184fd202fe11d6e6806a730cb5cdf3cc2646af
Choi-Ji-Yun <cjy5025@naver.com>
2020-08-28 20:14:36 +0900

    Exception

---
+++ 객체지향프로그래밍/Practice/13주차_1.cpp
@@ -0,0 +1,29 @@
+/*#include<iostream>
+using namespace std;
+
+double divide(double a, double b)
+{
+	try {
+		if (!b)
+			throw exception("Cannot divide by zero!");
+		else
+			cout << "Quotient is " << a / b << "\n";
+	}
+	catch (exception e) {
+		cout << "Cannot divide by zero!" << "\n";
+	}
+
+	return a / b;
+}
+
+int main()
+{
+	double a, b;
+
+	cout << "Enter numerator and denominator: ";
+	cin >> a >> b;
+
+	divide(a, b);
+
+	return 0;
+}*/
