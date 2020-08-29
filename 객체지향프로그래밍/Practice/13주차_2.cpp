3147f664382f380d8e7528e196a476d1b0f5cc61
Choi-Ji-Yun <cjy5025@naver.com>
2020-08-29 19:11:08 +0900

    Reference

---
+++ 객체지향프로그래밍/Practice/13주차_2.cpp
@@ -0,0 +1,22 @@
+/*#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	for (int i = 2; i <= 20; i++)
+	{
+		cout.setf(ios::showpos);
+		cout.precision(5);
+		cout.width(10);
+		cout.right;
+		cout << log(i) << " ";
+		cout.setf(ios::showpos);
+		cout.precision(5);
+		cout.width(10);
+		cout.right;
+		cout << log10(i) << '\n';
+	}
+
+	return 0;
+}*/
