05f699bf06dc98701a3790b8fa998de8aa44a5cf
Choi-Ji-Yun <cjy5025@naver.com>
2020-09-05 16:03:06 +0900

    Class_Template

---
+++ 객체지향프로그래밍/Report/Report4_2.cpp
@@ -0,0 +1,17 @@
+/*#include<iostream>
+using namespace std;
+
+template <class MType> MType Min(MType x,MType y) {
+	if (x > y)
+		return y;
+	else
+		return x;
+}; // Min 템블리트 함수
+
+int main()
+{ 
+	cout << "작은 값: " << Min(3, 4) << endl;// int형 작은 값 출력
+	cout << "작은 값: " << Min('c','a') << endl; // char형 작은 값 출력
+	return 0;
+}
+*/
